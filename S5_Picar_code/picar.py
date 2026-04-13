072444a9a880241e41891b744a531bfdcc57f965
noky121 <Evan.Frappier@USherbrooke.ca>
2026-04-13 15:19:18 +0100

    code pas propre

--- S5_Picar_code/picar.py
+++ S5_Picar_code/picar.py
@@ -3,18 +3,22 @@ import back_wheels
 import time
 from SunFounder_Line_Follower import Line_Follower
 import ultrasonic
+import numpy as np
 
 
 class Picar():
-    max_speed = 25
-    max_speed_acc = 25
+    max_speed = 20
+    max_speed_acc = 20
 
     #0: acceleration avant, 1: acceleration arriere, 2: decceleration avant, 3: decceleration arriere
     acc_state = 0
     speed_car = 15
     last_turn = 15
+    last_turn_state = 0
     lost_counter = 0
+    angle = 0
     obstacle_detected = False
+    cmpt = 0
     PATTERNS_CENTER = [
     [0, 0, 1, 0, 0],
     [0, 1, 1, 1, 0]
@@ -28,6 +32,10 @@ class Picar():
     PATTERNS_SLIGHT_RIGHT = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 0]]
     PATTERNS_HARD_RIGHT = [[0, 0, 0, 0, 1], [0, 0, 0, 1, 1]]
     PATTERN_LOST = [[0, 0, 0, 0, 0]]
+    PATTERN_STOP = [[1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1]]
+    
 
     
     def __init__(self):
@@ -44,18 +52,23 @@ class Picar():
     def backward(self, speed):
         self.back_wheels.backward()
         self.back_wheels.speed = speed
+        self.speed_car = speed
         
     def acceleration(self, speed, direction):
+        #self.cmpt += 1
         if(self.speed_car < speed):
-            self.speed_car += 2
+            self.cmpt = 0
+            self.speed_car += 1
             if direction == "forward":
                 self.forward(self.speed_car)
             elif direction == "backward":
                 self.backward(self.speed_car)
             
     def decceleration(self, speed, direction):
+        #self.cmpt += 1
         if(self.speed_car > speed):
-            self.speed_car -= 2
+            #self.cmpt = 0
+            self.speed_car -= 1
             if direction == "forward":
                 self.forward(self.speed_car)
             elif direction == "backward":
@@ -82,91 +95,104 @@ class Picar():
         if status in self.PATTERNS_CENTER:
             print("center")
             self.lost_counter = 0
+        
             self.max_speed_acc = self.max_speed
+            
+            if self.angle < 2:
+                self.angle += 2
+            elif self.angle > -2:
+                self.angle -= 2
+            else:
+                self.angle = 0
+            
             if direction == "forward":
               self.acc_state = 0
-              self.turn_while_moving(0, self.speed_car, "forward")
+              #self.angle = 0
+              self.turn_while_moving(self.angle, self.speed_car, "forward")
             else:
               self.acc_state = 1
-              self.turn_while_moving(0, self.speed_car, "backward")
+              #self.angle = 0
+              self.turn_while_moving(self.angle, self.speed_car, "backward")
 
         elif status in self.PATTERNS_SLIGHT_LEFT:
             print("slight left")
             self.lost_counter = 0
             self.max_speed_acc = self.max_speed
-            angle = -15 if direction == "forward" else 15
-            self.turn_while_moving(angle, self.speed_car, direction)
-            self.last_turn = angle
+            #angle = -15 if direction == "forward" else 15
+            if direction == "forward":
+                self.angle = np.clip(self.angle - 1, -17, 17)
+                self.acc_state = 0
+            else:
+                #self.angle = np.clip(self.angle + 1, -3, 3)
+                self.angle = 0
+                self.acc_state = 1
+            self.turn_while_moving(self.angle, self.speed_car, direction)
+            self.last_turn = self.angle
 
         elif status in self.PATTERNS_HARD_LEFT:
             print("hard left")
             self.lost_counter = 0
-            self.max_speed_acc = self.max_speed - 5
+            self.max_speed_acc = self.max_speed
+            #self.angle
             if direction == "forward":
-                angle = -25
+                if self.angle > 0:
+                  self.angle = 0
+                self.angle = np.clip(self.angle - 3, -65, 65)
                 self.acc_state = 0
             else:
-                angle = 40
+                #self.angle = np.clip(self.angle + 1, -5, 5)
+                self.angle = 0
                 self.acc_state = 1
-            self.turn_while_moving(angle, self.speed_car, direction)
-            self.last_turn = angle
+            self.turn_while_moving(self.angle, self.speed_car, direction)
+            self.last_turn = self.angle
 
         elif status in self.PATTERNS_SLIGHT_RIGHT:
             print("slight right")
             self.lost_counter = 0
             self.max_speed_acc = self.max_speed
             if direction == "forward":
-                angle = 15
+                self.angle = np.clip(self.angle + 1, -17, 17)
                 self.acc_state = 0
             else:
-                angle = -15
+                #self.angle = np.clip(self.angle - 1, -3, 3)
+                self.angle = 0
                 self.acc_state = 1
-            self.turn_while_moving(angle, self.speed_car, direction)
-            self.last_turn = angle
+            self.turn_while_moving(self.angle, self.speed_car, direction)
+            self.last_turn = self.angle
 
         elif status in self.PATTERNS_HARD_RIGHT:
             print("hard right")
             self.lost_counter = 0
-            self.max_speed_acc = self.max_speed - 5
+            self.max_speed_acc = self.max_speed
             if direction == "forward":
-                angle = 25
+                if self.angle < 0:
+                  self.angle = 0
+                self.angle = np.clip(self.angle + 3, -65, 65)
                 self.acc_state = 0
             else:
-                angle = -40
+                #self.angle = np.clip(self.angle - 1, -3, 3)
+                self.angle = 0
                 self.acc_state = 1
-            self.turn_while_moving(angle, self.speed_car, direction)
-            self.last_turn = angle
+            self.turn_while_moving(self.angle, self.speed_car, direction)
+            self.last_turn = self.angle
 
         elif status in self.PATTERN_LOST:
             print(self.lost_counter)
             print("lost")
-            if self.lost_counter >= 1:
+            if self.lost_counter >= 5:
                 self.lost_counter = 0
                 return 1
-              #recovery_direction = "backward" if direction == "forward" else "forward"
-              #self.decceleration(17)
-              #self.stop()
-              #self.forward(0)
-              
-              #time.sleep(0.4)
-              #cmpt = 0
-              #print(recovery_direction)
-              #while(self.line_follower.read_digital() in self.PATTERN_LOST):
-                #cmpt+
-                #if self.speed_car <= 25:
-                  #self.speed_car +=2
-                #self.turn_while_moving((self.last_turn * -1), self.speed_car, "backward")
-                #time.sleep(0.4)
-                #return 1              
-              #self.stop()
-              #self.front_wheels.turn(90 + self.last_turn)
-              #self.acceleration(25)
-              #time.sleep(0.1)
-              #self.lost_counter = 0
-              #self.speed_car = self.max_speed
             else:
-              #self.turn_while_moving((self.last_turn), self.max_speed - 7, direction)
-              self.lost_counter+=1
+                #self.angle = 0
+                if self.last_turn > 0:
+                    self.last_turn += 5    
+                else:
+                    self.last_turn -= 5
+                    
+                self.angle =  self.last_turn
+                self.turn_while_moving(self.angle, self.speed_car, direction)
+                self.lost_counter+=1
+                
         return 0
             
 
@@ -186,8 +212,11 @@ def test():
 
     try:
       while True:
-        time.sleep(0.2)
+        time.sleep(0.01)
         print(car.speed_car)
+        
+        
+        print(car.angle)
 
         match car.acc_state:
             case 0:
@@ -212,6 +241,14 @@ def test():
 
                 if hasNoPath == 1:
                     state = 5
+                if car.line_follower.read_digital() in car.PATTERN_STOP:
+                    car.speed_car -=2
+                    time.sleep(0.2)
+                    car.speed_car -=2
+                    time.sleep(0.2)
+                    car.stop()
+                    time.sleep(10)
+                    
 
             case 1: #etat 1 : Si obstacle detecte, descelerrer jusqua 10 cm
                 print("state 1")
@@ -247,7 +284,7 @@ def test():
                 start = time.time()
                 #car.speed_car = round(car.max_speed * 0.8)
                 print(start)
-                while((time.time() - start) <= 2.4):
+                while((time.time() - start) <= 2.5):
                   print("in while")
                   if car.speed_car <= car.max_speed:
                     car.speed_car += 1
@@ -261,7 +298,7 @@ def test():
             case 4: #contourner lobjet
                 print("state 4")
                 cmpt = 0
-                while(cmpt < 1):
+                while(cmpt < 2):
                   if car.speed_car >= 23:
                     car.speed_car -= 1
                     
@@ -279,29 +316,23 @@ def test():
             case 5: #etat 5: deccelerer pour retrouver la ligne
                 print("state 5")
                 car.acc_state = 2
-                car.max_speed_acc = 19
+                car.max_speed_acc = 10
                 if car.speed_car <= car.max_speed_acc:
                     car.stop()
                     time.sleep(0.5)
                     state = 6
 
-            case 6: #etat 6: revuler pour reprendre la ligne
+            case 6: #etat 6: reculer pour reprendre la ligne
                 print("state 6")
-                if car.line_follower.read_digital() not in car.PATTERN_LOST:
+                if car.line_follower.read_digital() in car.PATTERNS_SLIGHT_LEFT or car.line_follower.read_digital() in car.PATTERNS_SLIGHT_RIGHT or car.line_follower.read_digital() in car.PATTERNS_CENTER:
                     car.stop()
                     time.sleep(0.5)
                     state = 0
                 else:
                     car.acc_state = 1
-                    car.max_speed_acc = 20
+                    car.max_speed_acc = 22
                     car.turn_while_moving((car.last_turn * -1), car.speed_car, "backward")
 
-                
-                
-              
-                    
-
-        #etat 4: Evitement d'obstacle
         
     except KeyboardInterrupt:
         car.stop()
@@ -311,10 +342,41 @@ def stop_test():
   car = Picar()
   car.stop()
 
-
+def reculons():
+      car = Picar()
+      car.stop()
+      car.speed_car = 22
+      car.front_wheels.turn(90)
+      while(car.line_follower.read_digital() not in car.PATTERN_STOP):
+         # match car.acc_state:
+          #  case 0:
+           #     car.acceleration(car.max_speed_acc, "forward")
+            #case 1:
+             #   car.acceleration(car.max_speed_acc, "backward")
+            #case 2:
+            #    car.decceleration(car.max_speed_acc, "forward")
+            #case 3:
+             #   car.decceleration(car.max_speed_acc, "backward")
+                
+          #car.line_following("backward")
+          #acc_state = 1
+          car.backward(car.speed_car)
+          time.sleep(0.1)
+          
+          
+      car.speed_car -=2
+      car.backward(car.speed_car)
+      time.sleep(0.4)
+      car.speed_car -=2
+      car.backward(car.speed_car)
+      time.sleep(0.4)
+      car.stop()
+      
+    
 
 if __name__ == '__main__':
     try:
-      test()
+      #test()
+      reculons()
     finally:
       stop_test()
